38d67edb208dc4581b6193e296ec72ad83d8b6a9
your username <huynhduc290698@gmail.com>
2021-08-26 20:45:20 +0700

    commit

--- bai7_ service_router/exercise/application-dictionary/src/app/app.module.ts
+++ bai7_ service_router/exercise/application-dictionary/src/app/app.module.ts
@@ -2,13 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CreateComponent } from './dictionary/create/create.component';
+import { EditComponent } from './dictionary/edit/edit.component';
+import { ListComponent } from './dictionary/list/list.component';
+import { DetailComponent } from './dictionary/detail/detail.component';
+import { DictionaryListComponent } from './dictionary/dictionary-list/dictionary-list.component';
+import { ViewComponent } from './dictionary/view/view.component';
+import { DictionaryCreateComponent } from './dictionary-create/dictionary-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateComponent,
+    EditComponent,
+    ListComponent,
+    DetailComponent,
+    DictionaryListComponent,
+    ViewComponent,
+    DictionaryCreateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    MatDialogModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
